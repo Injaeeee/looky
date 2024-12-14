93d701438a2adbd018d95bd0718436dffce5ff30
Injaeeee <97098671+Injaeeee@users.noreply.github.com>
2024-12-15 04:14:18 +0900

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./router/rootLayout";
+import MainPage from "./pages/mainPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
