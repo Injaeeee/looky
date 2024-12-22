dc144e4c49f36da766b293a40d2b39526993bfac
Injaeeee <97098671+Injaeeee@users.noreply.github.com>
2024-12-23 04:41:12 +0900

    feat : 목록페이지 퍼블리싱

--- src/assets/style/GlobalStyle.jsx
+++ src/assets/style/GlobalStyle.jsx
@@ -1,47 +1,8 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-/* font */
-@font-face {
-  font-family: 'Pretendard';
-  src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
-    format('woff');
-  font-weight: 400;
-  font-style: normal;
-}
 
-@font-face {
-  font-family: 'Pretendard';
-  src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff')
-    format('woff');
-  font-weight: 700;
-  font-style: normal;
-}
 
-@font-face {
-  font-family: 'Noto Sans';
-  font-style: normal;
-  font-weight: 400;
-  src: url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff2) format('woff2'),
-  url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff) format('woff'),
-  url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.otf) format('opentype');
-}
-
-@font-face {
-    font-family: '나눔명조';
-    src: url('https://cdn.jsdelivr.net/gh/wizfile/font/NanumMyeongjo.eot');
-    src:url('https://cdn.jsdelivr.net/gh/wizfile/font/NanumMyeongjo.woff') format('woff');
-    font-style: normal;
-}
-
-@font-face {
-    font-family: '나눔손글씨 손편지체';
-    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/naverfont_05@1.0/Handletter.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-}
-
-/*  default */
 * {
   box-sizing: border-box;
   margin: 0;
@@ -55,57 +16,13 @@ const GlobalStyle = createGlobalStyle`
 
 html {
   font-size: 62.5%; /* 단위 rem 사용 시 16px = 1.6rem으로 사용 가능 */
-}
-
-/* 여백초기화 */
-body,
-div,
-ul,
-li,
-dl,
-dd,
-dt,
-ol,
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-input,
-fieldset,
-legend,
-p,
-select,
-table,
-th,
-td,
-tr,
-textarea,
-button,
-form,
-figure,
-figcaption {
-  margin: 0;
-  padding: 0;
-}
+  
+} 
 
 /* a 링크 초기화 */
 a {
   color: #222;
   text-decoration: none;
-}
-/* 폰트 스타일 초기화 */
-em,
-address {
-  font-style: normal;
-}
-
-/* 블릿기호 초기화 */
-ul,
-li,
-ol {
-  list-style: none;
 }
 
 /* 제목 태그 초기화 */
@@ -146,6 +63,11 @@ input::-webkit-inner-spin-button {
   --error: #dc3a3a;
   --surface: #f6f8ff;
 
+  --pink100:#F76388;
+  --pink200:#d44f70;
+  --pink300: #ad2c4d;
+  --pink400:#912c45;
+
   --purple100: #f8f0ff;
   --purple200: #ecd9ff;
   --purple300: #dcb9ff;
@@ -177,10 +99,10 @@ input::-webkit-inner-spin-button {
   --gray100: #f6f6f6;
   --gray200: #eeeeee;
   --gray300: #cccccc;
-  --gray400: #999999;
+  --gray400: #D9D9D9;
   --gray500: #555555;
   --gray600: #4a4a4a;
-  --gray700: #3a3a3a;
+  --gray700: #333333;
   --gray800: #2b2b2b;
   --gray900: #181818;
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,121 @@
+import { useState, useEffect } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+import { Avatar, Image } from "@chakra-ui/react";
+
+export default function Header() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const { postId } = useParams();
+  const location = useLocation();
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const excludedPaths = ["/", "/list"];
+  const detailPagePath = `/post/${postId}`;
+
+  const existingPath = excludedPaths.includes(location.pathname);
+  const existingDetailpath = detailPagePath === location.pathname;
+
+  if (isMobile && !existingPath) {
+    return null;
+  }
+
+  return (
+    <HeaderContainer>
+      <Navigation>
+        <UserWrapper>
+          <Avatar name="Oshigaki Kisame" src="https://bit.ly/broken-link" />
+
+          <UserName>@injae</UserName>
+        </UserWrapper>
+        <div>
+          <Link to="/">
+            <Image src="/image/logo.png" alt="logo" />
+          </Link>
+        </div>
+        <RouterWrapper>
+          <CreateButton>
+            <Image src="/icon/create.svg" alt="create" />
+            CREATE
+          </CreateButton>
+          <RouterButton>
+            <Image src="/icon/article.svg" alt="article" />
+            게시물
+          </RouterButton>
+          <RouterButton>
+            <Image src="/icon/ranking.svg" alt="ranking" />
+            랭킹
+          </RouterButton>
+        </RouterWrapper>
+      </Navigation>
+    </HeaderContainer>
+  );
+}
+
+const HeaderContainer = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  border-bottom: 1px solid var(--pink100);
+  background: #0f0f0f;
+  z-index: 10;
+`;
+
+const Navigation = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1200px;
+  height: 64px;
+  margin: 0 auto;
+
+  @media (max-width: 1248px) {
+    padding: 0 24px;
+  }
+  @media (max-width: 640px) {
+    padding: 0 16px;
+  }
+`;
+
+const UserWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const UserName = styled.p`
+  font-size: 12px;
+  font-weight: 600;
+`;
+
+const RouterWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const RouterButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-size: 14px;
+  font-weight: 600;
+  color: var(--pink100);
+`;
+
+const CreateButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-size: 14px;
+  font-weight: 700;
+  color: var(--gray100);
+  background-color: var(--gray600);
+  border-radius: 24px;
+  padding: 8px 16px;
+`;
